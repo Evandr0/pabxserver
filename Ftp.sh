ee517aa54e3733a7f4b9c3a9fe296b5151203331
Evandr0 <evandro@live.jp>
2022-06-06 13:43:05 -0300

    atualização do arquivo

--- Ftp.sh
+++ Ftp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Instala servidor proftpd e SNGREP
+#Comando será feito na produção para instalação do proftpd e SNGREP.
 set -e
 
 #Disable Selinux Temporarily
@@ -16,18 +18,27 @@ if [ -e $sefile ]
 then
   sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 fi
-##################################################################
-yum -y install proftpd
-useradd pabxserver -d /home/pabxserver
-passwd pabxserver
-chown -R pabxserver:pabxserver /home/pabxserver/
+
+#Instal proftpd
+yum -y install proftpd &
+pid=$!
+wait $pid
+echo $pid was terminated.
+yum -y install sngrep &
+pid=$!
+wait $pid
+echo Instalação sngrep terminou
+
 # Enable and Start Firewall
-firewall-cmd --add-service=ftp --permanent --zone=public
+firewall-cmd --add-service=ftp --permanent --zone=public &
+pid=$!
+wait $pid
+echo $pid was terminated.
 firewall-cmd --reload
-# Enable and Start Proftpd
+#Enable and Start Proftpd
 systemctl start proftpd
 systemctl enable proftpd
-###################################################################
+
 # Reboot System to Make Selinux Change Permanently
 echo "Rebooting System"
 reboot
