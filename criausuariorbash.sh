6396eee6e16d0a806293b22c6735ceb515c8a486
Evandr0 <evandro@live.jp>
2022-06-08 11:31:24 -0300

    modify

--- criausuariorbash.sh
+++ criausuariorbash.sh
@@ -13,28 +13,28 @@ else
 fi
 #adicionar usuario rbash nome pabxserver
 #useradd maria1 -s /bin/rbash; (echo maria123; echo maria123) | passwd maria1 
-useradd pabxserver -s /bin/rbash; (echo pabxserver@intelbras; echo pabxserver@intelbras) | passwd pabxserver
-mkdir -p /home/pabxserver/bin
-mkdir -p /home/pabxserver/sbin
+useradd pabxserver3 -s /bin/rbash; (echo pabxserver@intelbras; echo pabxserver@intelbras) | passwd pabxserver3
+mkdir -p /home/pabxserver3/bin
+mkdir -p /home/pabxserver3/sbin
 
 #Preciso editar o arquivo /home/pabxserver/.bash_profile linha onde temosPATH=$PATH:$HOME/.local/bin:$HOME/bin alterar para PATH=$HOME/bin
-sed -i ' s/^PATH.*/PATH=$HOME\/bin/' /home/pabxserver/.bash_profile
+sed -i ' s/^PATH.*/PATH=$HOME\/bin/' /home/pabxserver3/.bash_profile
 
 ##Permissões para o usuario pabxserver
-sudo chown root:root /home/pabxserver/.bash_profile
-sudo chmod 755 /home/pabxserver/.bash_profile
-chown -R pabxserver:pabxserver /home/pabxserver/
+sudo chown root:root /home/pabxserver3/.bash_profile
+sudo chmod 755 /home/pabxserver3/.bash_profile
+chown -R pabxserver3:pabxserver3 /home/pabxserver3/
 ##
 
 #Dando permissões especificas.
 
-ln -s /bin/ls /home/pabxserver/bin/
-ln -s /bin/passwd /home/pabxserver/bin/
-ln -s /bin/top /home/pabxserver/bin/
-ln -s /bin/uptime /home/pabxserver/bin/
-ln -s /bin/sngrep /home/pabxserver/bin/
-ln -s /bin/tcpdump /home/pabxserver/bin/
-ln -s /bin/sudo /home/pabxserver/bin/
-ln -s /sbin/reboot /home/pabxserver/sbin/
-ln -s /sbin/ifconfig /home/pabxserver/sbin/
-sed -i '$a pabxserver ALL=NOPASSWD:/usr/bin/sngrep,/usr/sbin/reboot,/home/pabxserver/criausuarioftp.sh,/usr/sbin/ifconfig,/home/pabxserver/senha.sh' /etc/sudoers
+ln -s /bin/ls /home/pabxserver3/bin/
+ln -s /bin/passwd /home/pabxserver3/bin/
+ln -s /bin/top /home/pabxserver3/bin/
+ln -s /bin/uptime /home/pabxserver3/bin/
+ln -s /bin/sngrep /home/pabxserver3/bin/
+ln -s /bin/tcpdump /home/pabxserver3/bin/
+ln -s /bin/sudo /home/pabxserver3/bin/
+ln -s /sbin/reboot /home/pabxserver3/sbin/
+ln -s /sbin/ifconfig /home/pabxserver3/sbin/
+sed -i '$a pabxserver3 ALL=NOPASSWD:/usr/bin/sngrep,/usr/sbin/reboot,/home/pabxserver3/criausuarioftp.sh,/usr/sbin/ifconfig,/home/pabxserver3/senha.sh' /etc/sudoers
