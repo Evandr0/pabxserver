34046ab9db2ec03ab603ca1c4613d5b7e4328af9
Evandr0 <evandro@live.jp>
2022-07-01 10:03:53 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # pabxserver
-Script install_pabxserver.sh - Instala todo o sistema do PABX SERVER.
 Script criausuariorbash - Cria um usuário com restrições de comandos.
 Script criausuarioftp.sh - Será usado pelo usuario rbash para criar um usuario pro ftp.
 Script criausuariodevel.sh - Cria o usuario devel, user não pode fazer login com senha, somente com privatekey. (O user devel pode digitar o comando para logar como root)
+Script logs.sg - Será usado pelo usuario para copiar os arquivos de log e colocar na pasta /home/ftp/bilhetes
 
 #User devel.
 arquivos public key e private key estão na pasta sshkeygen
@@ -16,10 +16,19 @@ Teste realizado com Bitvise SSH Client 9.16:
 - Senha padrão pabxserver@intelbras
 
 Comandos disponíveis.
-ls, top, uptime, passwd, lsmod, dmesg, lsusb, df, ping, traceroute,
+ls, top, uptime, passwd, lsmod, dmesg, lsusb, df, ping, traceroute
 
 Comandos disponíveis com sudo
-ifconfig, sngrep, ./criausuarioftp.sh, reboot, intelbras, ./update.sh
+ifconfig, sngrep, reboot, intelbras, route, ./criausuarioftp.sh, ./update.sh, ./logs.sh, ./cleanall.sh
+#Ainda falta script HA
+comando ifconfig
+        Usado para configuração das interfaces de rede.
+
+Comando sngrep
+        Usado para debug/caputura do SIP em tempo real.
+
+Comando reboot
+        Usado para reiniciar o servidor
 
 Comando intelbras
         reset-pwd [username]    (Comando para reset da senha do usuáruio WEB.)
@@ -32,4 +41,11 @@ Comando intelbras
         restore-backup          (Restaura um backup salvo, necessário especificar o caminho do arquivo | /var/lib/vitalpbx/backup/ )
         reset-apache-conf       (Restaura as configurações do apache, geralmente utilizado quando tiver algum problema com certificado https)
 
-Sobre o SNGREP. Pasta padrão de salvamento será a pasta /usr/share/vitalpbx/www/ isso faz com que seja possível baixar o arquivo diretamente do navegador WEB acessando o IP do equipamento/nomedoarquivo.pcap
+Comando route
+        Faz alteração de rotas no linux.
+
+Comando ./criausuarioftp.sh
+        Cria o usuario bilhetes para o FTP. (Necessário para configuração de tarifação/controller)
+
+
+Sobre o SNGREP. Pasta padrão de salvamento será a pasta /usr/share/vitalpbx/www/ isso faz com que seja possível baixar o arquivo diretamente do navegador WEB acessando o IP do equipamento/nomedoarquivo.pcap - Criado link simbólico /home/pabxserver/logs
---
+++ criausuariorbash.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Site: https://git.intelbras.com.br/ev047953/pabxserver
+# Autor: Evandro <evandro.dias@Intelbras.com.br>
+##############################################################
+#https://maisgeek.com/como-usar-o-shell-restrito-para-limitar-o-que-um-usuario-linux-pode-fazer/
+#Script cria usuario rbash limitado para uso de alguns comandos.
+###############################################################
+
+
+
+#Verifica se o caminho rbash existe, cria ele se não existir.
+
+ln -s /bin/bash /bin/rbash
+sleep 1
+
+#adicionar usuario rbash nome pabxserver
+#useradd maria1 -s /bin/rbash; (echo maria123; echo maria123) | passwd maria1
+
+sudo useradd pabxserver -d /home/pabxserver -m -s /bin/rbash && (echo pabxserver@intelbras; echo pabxserver@intelbras) | passwd pabxserver
+sleep 2
+cp /etc/skel/.* /home/pabxserver/
+mkdir -p /home/pabxserver/bin
+mkdir -p /home/pabxserver/sbin
+mkdir -p /home/pabxserver/logs
+
+#Preciso editar o arquivo /home/pabxserver/.bash_profile linha onde temosPATH=$PATH:$HOME/.local/bin:$HOME/bin alterar para PATH=$HOME/bin
+echo "verificando se o arquivo bash_profile existe"
+ls -a /home/pabxserver/
+sed -i 's/^PATH.*/PATH=$HOME\/bin/' /home/pabxserver/.bash_profile
+
+##Permissões para o usuario pabxserver
+sudo chown root:root /home/pabxserver/.bash_profile
+sudo chmod 755 /home/pabxserver/.bash_profile
+chown -R pabxserver:pabxserver /home/pabxserver/criausuarioftp.sh
+##
+
+#Dando permissões especificas.
+#Alguns comandos para diagnósticos e debug.
+ln -s /bin/dmesg /home/pabxserver/bin/
+ln -s /bin/ping /home/pabxserver/bin/
+ln -s /bin/traceroute /home/pabxserver/bin/
+ln -s /sbin/lsmod /home/pabxserver/bin/
+ln -s /bin/lsusb /home/pabxserver/bin/
+ln -s /bin/df /home/pabxserver/bin/
+ln -s /bin/asterisk /home/pabxserver/bin/
+ln -s /sbin/asterisk /home/pabxserver/sbin/
+ln -s /bin/ls /home/pabxserver/bin/
+ln -s /bin/passwd /home/pabxserver/bin/
+ln -s /bin/top /home/pabxserver/bin/
+ln -s /bin/uptime /home/pabxserver/bin/
+ln -s /bin/sngrep /home/pabxserver/bin/
+ln -s /bin/tcpdump /home/pabxserver/bin/
+ln -s /bin/sudo /home/pabxserver/bin/
+ln -s /sbin/reboot /home/pabxserver/sbin/
+ln -s /sbin/ifconfig /home/pabxserver/sbin/
+ln -s /usr/sbin/intelbras /home/pabxserver/sbin/
+ln -s /usr/sbin/route /home/pabxserver/sbin/
+ln -s /home/pabxserver/logs /usr/share/vitalpbx/www/
+sed -i '$a pabxserver ALL=NOPASSWD:/usr/bin/sngrep,/home/pabxserver/cleanall.sh,/usr/sbin/route,/usr/sbin/reboot,/home/pabxserver/criausuarioftp.sh,/usr/sbin/ifconfig,/home/pabxserver/sbin/intelbras,/home/pabxserver/update.sh,/home/pabxserver/logs.sh,/usr/sbin/asterisk' /etc/sudoers
+
+
+############################
+############################
